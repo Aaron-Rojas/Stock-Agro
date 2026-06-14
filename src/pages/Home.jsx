6e299333690f19cc7cc3f79fb9bf4295548434c4
CarlosCorrea <correacarlos1003@gmai.com>
2026-06-14 14:32:32 -0500

    Commit de verificacion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,35 @@
-import React from 'react';
-import { Contenedor } from '../components/atoms/Contenedor';
-import { useAccesibilidad } from '../hooks/useAccesibilidad';
-import { MenuOpciones } from '../components/organisms/MenuOpciones';
+import React from "react";
+import { Contenedor } from "../components/atoms/Contenedor";
+import { useAccesibilidad } from "../hooks/useAccesibilidad";
+import { MenuOpciones } from "../components/organisms/MenuOpciones";
 
 export const Home = () => {
   const { temaActual, nivelLetra } = useAccesibilidad();
 
-  const tamanoTitulo = nivelLetra === 'normal' ? '30px' : nivelLetra === 'grande' ? '38px' : '45px';
+  const tamanoTitulo =
+    nivelLetra === "normal"
+      ? "30px"
+      : nivelLetra === "grande"
+        ? "38px"
+        : "45px";
 
   return (
     <Contenedor>
-        <div style={{ padding: '30px', textAlign: 'center' }}>
-            <h1 style={{ 
-            color: temaActual.textoPrincipal, 
-            fontSize: tamanoTitulo, 
-            fontFamily: 'monospace',
-            letterSpacing: '3px',
-            margin: 0
-            }}>
-            INICIO
-            </h1>
+      <div style={{ padding: "30px", textAlign: "center" }}>
+        <h1
+          style={{
+            color: temaActual.textoPrincipal,
+            fontSize: tamanoTitulo,
+            fontFamily: "monospace",
+            letterSpacing: "3px",
+            margin: 0,
+          }}
+        >
+          INICIO
+        </h1>
       </div>
 
       <MenuOpciones />
-         
     </Contenedor>
   );
 };
